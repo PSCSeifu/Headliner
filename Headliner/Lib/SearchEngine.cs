685832c39e7d7401c66395186ee6333e454e0fec
seifu <seifu@SEIFU-TOLOSA>
2016-12-07 16:55:11 +0000

    Added search box and class

--- Headliner/Lib/SearchEngine.cs
+++ Headliner/Lib/SearchEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AngleSharp.Dom.Html;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,19 @@ namespace Headliner.Lib
 {
     public class SearchEngine
     {
-        
+        public static List<string> SearchByWord(IHtmlDocument doc, string searchWord)
+        {
+            var joined = doc.GetElementsByClassName("title").Select(x => x.TextContent)
+                .Union(doc.GetElementsByTagName("a href").Select(x => x.TextContent))
+                .Union(doc.GetElementsByClassName("main-promobox__link").Select(x => x.TextContent)
+                ).ToList();
+            List<string> filterd = new List<string>();
+
+            foreach (var item in joined)
+            {
+                filterd.Add(item);
+            }
+            return filterd;
+        }
     }
 }
